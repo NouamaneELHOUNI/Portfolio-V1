a35d9d8c3ef8ed75453051141b7da5e6c4639ebe
Itsme.Ray <nouamane.elhouni.2002@gmail.com>
2024-02-26 20:53:20 +0100

    deploy test

--- pages/about.jsx
+++ pages/about.jsx
@@ -2,6 +2,7 @@ const AboutPage = () => {
   return (
     <>
       <h3>A Little Bit About Me</h3>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam aspernatur ut fugit, illum dolores eaque beatae accusamus dignissimos animi maxime, repellat praesentium exercitationem nemo explicabo sed soluta illo, voluptatem perferendis?
     </>
   );
 };
